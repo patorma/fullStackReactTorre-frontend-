b02e52bc3b334eeb368ca296e03e841f4b327ec7
patorma <pocontreras@ing.ucsc.cl>
2020-03-09 00:23:54 -0300

    Se termino el formulario del login(sin validaciones y sin enviar datos)

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
+     // State para iniciar sesión
+     const [usuario,guardarUsuario] = useState({
+        email: '',
+        password:''
+    });
+    // extraer de usuario
+     const {email, password} = usuario;
+
+    const onChange = (e)=>{
+       // se va a guardar en el state
+       guardarUsuario({
+           ...usuario, // hacemos una copia de usuario
+            [e.target.name] : e.target.value// reescribimos con los valores ingresados en input
+       })
+    }
+
+    // Cuando el usuario quiere iniciar sesión
+    const onSubmit = e =>{
+        e.preventDefault();
+
+        // Validar que no haya campos vacios
+
+        // Pasarlo al action 
+
+    }
     return (
-         <h1> Desde Login </h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+
+                <form 
+                 onSubmit={onSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Tu Email"
+                        value={email}
+                        onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Tu Password"
+                        value={password}
+                        onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block"
+                         value="Iniciar Sesión"
+                        />
+                    </div>
+                </form>
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Obtener Cuenta
+                </Link>
+            </div>
+        </div>
     );
 }
 
