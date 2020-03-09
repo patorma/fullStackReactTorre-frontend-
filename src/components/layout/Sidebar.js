162a1f771c553335085e7d69eed99e20d142ff20
patorma <pocontreras@ing.ucsc.cl>
2020-03-09 02:07:34 -0300

    Inicio de implementacion de nuevos proyectos

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return ( 
+        <aside>  {/** se crea formulario para ingresar proyectos*/}
+            <h1>MERN<span>Tasks</span></h1>
+
+            <div className="proyectos">
+                <h2>Tus proyectos</h2>
+            </div>
+        </aside>
+     );
+}
+ 
+export default Sidebar;
