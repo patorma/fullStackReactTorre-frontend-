18d56f2d571c79d72217c9e36a35bcde140ee9f5
patorma <pocontreras@ing.ucsc.cl>
2020-03-10 01:48:54 -0300

    se creo interfaz de tareas (sin funcionalidad)

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Sidebar from '../layout/Sidebar';
 import Barra from '../layout/Barra';
+import FormTarea from '../tareas/FormTarea';
+import ListadoTareas from '../tareas/ListadoTareas';
 
 const Proyectos = () => {
     return ( 
@@ -10,8 +12,9 @@ const Proyectos = () => {
            <div className="seccion-principal">
                <Barra />
                 <main>
+                    <FormTarea />
                     <div className="contenedor-tareas">
-
+                        <ListadoTareas/>
                     </div>
                 </main>
            </div>
